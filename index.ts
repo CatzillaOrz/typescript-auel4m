375845609ad985b0357633e91387e555eb133925
CatzillaOrz <catzillaorz@outlook.com>
2022-07-26 16:00:52 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -1,11 +1,27 @@
 class ProjectInput {
   templateElment: HTMLTemplateElement;
-  hostElment: HTMLDivElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
 
   constructor() {
     this.templateElment = document.getElementById(
       'project-input'
     )! as HTMLTemplateElement;
-    this.hostElment = document.getElementById('app')! as HTMLDivElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    /*
+     **  mportNode(importedNode: DocumentFragment, deep: boolean): DocumentFragment
+     ** Returns a copy of node. If deep is true, the copy also includes the node's descendants.
+     **
+     */
+    const importedNode = document.importNode(this.templateElment.content, true);
+
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
 }

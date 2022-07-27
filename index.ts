81a317b972934542ec37498ca0662e518cd73d2f
CatzillaOrz <catzillaorz@outlook.com>
2022-07-27 17:20:42 +0800

    2 files modified

--- index.ts
+++ index.ts
@@ -188,7 +188,32 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 /*
- **  [ProjectList]
+ **  [projectItem] extends Component
+ **
+ **
+ */
+
+class PorjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure(): void {}
+  renderContent(): void {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent =
+      this.project.people.toString();
+    this.element.querySelector('p')!.textContent = this.project.description;
+  }
+}
+
+/*
+ **  [ProjectList] extends Component
  **
  **
  */
@@ -209,10 +234,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     )! as HTMLUListElement;
     listEl.innerHTML = '';
     for (const projItem of this.assignedProjects) {
-      const listItem = document.createElement('li');
-      listItem.textContent = projItem.title;
-      listItem.className = 'list-group-item';
-      listEl.appendChild(listItem);
+      new PorjectItem(this.element.querySelector('ul')!.id, projItem);
     }
   }
 

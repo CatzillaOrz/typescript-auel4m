6f208fd2903fc5a8c51b5937d2f41a129e3830ff
CatzillaOrz <catzillaorz@outlook.com>
2022-07-27 17:25:19 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -195,6 +195,14 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 
 class PorjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
+
+  get persoon() {
+    if (this.project.people === 1) {
+      return `1 Person`;
+    } else {
+      return `${this.project.people} Persons`;
+    }
+  }
   constructor(hostId: string, project: Project) {
     super('single-project', hostId, false, project.id);
     this.project = project;
@@ -206,8 +214,7 @@ class PorjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   configure(): void {}
   renderContent(): void {
     this.element.querySelector('h2')!.textContent = this.project.title;
-    this.element.querySelector('h3')!.textContent =
-      this.project.people.toString();
+    this.element.querySelector('h3')!.textContent = this.persoon;
     this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
